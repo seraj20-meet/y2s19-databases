8454fa29ed70ba62b31cec9cf00cc44fd462e064
myname21-meet <myname21@meet.mit.edu>
2019-07-29 03:31:45 -0400

    nice class

--- knowledge_model.py
+++ knowledge_model.py
@@ -3,15 +3,45 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy import create_engine
 
-Base = declarative_base()
+#Base = declarative_base()
 
-class Knowledge(Base):
+#class Knowledge(Base):
 	# Create a table with 4 columns
 	# The first column will be the primary key
 	# The second column should be a string representing
 	# the name of the Wiki article that you're referencing
 	# The third column will be a string representing the 
 	# topic of the article. The last column will be
-	# an integer, representing your rating of the article.
+	# an integer, representing your rating of the article
 
-	pass
+
+Base = declarative_base()
+
+
+class Food(Base):
+   __tablename__ = 'food'
+   food_name = Column(String, primary_key=True)
+   color = Column(String)
+   calories = Column(Integer)
+   taste = Column(String)
+   def __repr__ (self):
+   	#return("food Name: {}\n"
+#                "Student Year: {} \n"
+#                "Has Finished Lab: {}").format(
+#                     self.name, self.year, self.finished_lab) )
+# pizza=Food(food_name="pizza", color="orange")
+# print(pizza)
+
+# class Pilots(Base):
+#    __tablename__ = ''
+#    pilot_name = Column(String, primary_key=True)
+#    airplane = Column(String)
+#    age = Column(Integer)
+#    experience = Column(String)
+#    def __repr__ (self):
+#    	return( self.food_name)
+# pizza=Food(food_name="pizza", color="orange")
+# print(pizza)
+
+
+  
